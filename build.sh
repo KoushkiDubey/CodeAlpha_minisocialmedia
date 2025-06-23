080c2f40be8837511f28969dfc6ee4bb2ccd3988
KoushkiDubey <koushkidubey123@gmail.com>
2025-06-24 01:00:07 +0530

    initial submission

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "${DATABASE_URL}" ]; then
+  echo "ERROR: DATABASE_URL not set!" >&2
+  exit 1
+fi
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput  # Only for static files
