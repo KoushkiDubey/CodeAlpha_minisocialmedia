abfc68fb8badf56846740294980ff821c664be55
KoushkiDubey <koushkidubey123@gmail.com>
2025-06-24 16:42:51 +0530

    initial submission

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
     }
 }
 
-if not DEBUG:
+if 'DATABASE_URL' in os.environ:  # 👈 Changed from DEBUG check
     import dj_database_url
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
